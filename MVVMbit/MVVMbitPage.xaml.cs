ccfa898221ff9e9c635bb70af0f0fda4b7ad0524
June <junecho.ucb@gmail.com>
2018-02-20 15:15:49 -0800

    Initial commit

---
+++ MVVMbit/MVVMbitPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace MVVMbit
+{
+    public partial class MVVMbitPage : ContentPage
+    {
+        public MVVMbitPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
